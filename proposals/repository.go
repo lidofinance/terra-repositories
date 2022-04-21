aca1199b054fc484523b7023b7e655166595a9ce
nhpd <dmitry.klpv@gmail.com>
2022-04-22 00:24:40 +0300

    post-review fixes: modify fetch voting and deposit proposals funcs to use new api

--- proposals/repository.go
+++ proposals/repository.go
@@ -14,13 +14,11 @@ import (
 )
 
 var (
-	VotingStatus     = "Voting"
-	DepositStatus    = "Deposit"
 	votePageSize     = "1000"
 	proposalPageSize = "1000"
 
-	DepositStatus2 = "1"
-	VotingStatus2  = "2"
+	DepositStatus = "1"
+	VotingStatus  = "2"
 )
 
 func New(apiClient *client.TerraRESTApis) *Repository {
@@ -51,23 +49,13 @@ func (r *Repository) Get(ctx context.Context, proposalID int) (*models.GetPropos
 }
 
 // FetchVoting fetches proposals with voting status
-func (r *Repository) FetchVoting(ctx context.Context) ([]*models.GetProposalListResultProposals, error) {
+func (r *Repository) FetchVoting(ctx context.Context) ([]*query.ProposalsOKBodyProposalsItems0, error) {
 	return r.fetch(ctx, VotingStatus)
 }
 
 // FetchDeposit fetches proposals with deposit status
-func (r *Repository) FetchDeposit(ctx context.Context) ([]*models.GetProposalListResultProposals, error) {
-	return r.fetch(ctx, DepositStatus2)
-}
-
-// FetchVoting2 fetches proposals with voting status
-func (r *Repository) FetchVoting2(ctx context.Context) ([]*query.ProposalsOKBodyProposalsItems0, error) {
-	return r.fetch2(ctx, VotingStatus2)
-}
-
-// FetchDeposit2 fetches proposals with deposit status
-func (r *Repository) FetchDeposit2(ctx context.Context) ([]*query.ProposalsOKBodyProposalsItems0, error) {
-	return r.fetch2(ctx, DepositStatus2)
+func (r *Repository) FetchDeposit(ctx context.Context) ([]*query.ProposalsOKBodyProposalsItems0, error) {
+	return r.fetch(ctx, DepositStatus)
 }
 
 func (r *Repository) GetVotingProposalVotes(ctx context.Context, proposalID int) ([]*query.VotesOKBodyVotesItems0, error) {
@@ -100,28 +88,7 @@ func (r *Repository) GetVotingProposalVotes(ctx context.Context, proposalID int)
 	return votes, nil
 }
 
-func (r *Repository) fetch(ctx context.Context, status string) ([]*models.GetProposalListResultProposals, error) {
-	resp, err := r.apiClient.Governance.GetV1GovProposals(
-		&governance.GetV1GovProposalsParams{
-			Status:  &status,
-			Context: ctx,
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get proposals with {%s} status: %w", status, err)
-	}
-
-	err = resp.GetPayload().Validate(nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to validate proposals with {%s} status response: %w", status, err)
-	}
-
-	return resp.GetPayload().Proposals, nil
-}
-
-// Uses the more correct lcd API
-// TODO: check that return types correlate with swagger types (query.ProposalsOKBodyProposalsItems0)
-func (r *Repository) fetch2(ctx context.Context, status string) ([]*query.ProposalsOKBodyProposalsItems0, error) {
+func (r *Repository) fetch(ctx context.Context, status string) ([]*query.ProposalsOKBodyProposalsItems0, error) {
 	var paginationKey strfmt.Base64
 
 	res := make([]*query.ProposalsOKBodyProposalsItems0, 0)
@@ -136,12 +103,12 @@ func (r *Repository) fetch2(ctx context.Context, status string) ([]*query.Propos
 
 		resp, err := r.apiClient.Query.Proposals(&params)
 		if err != nil {
-			return res, fmt.Errorf("failed to get proposals with {%s} status: %s\"", err, status)
+			return nil, fmt.Errorf("failed to get proposals with status = %s: %w", status, err)
 		}
 
 		err = resp.GetPayload().Validate(nil)
 		if err != nil {
-			return nil, fmt.Errorf("failed to validate proposals response for status: %s: %w", status, err)
+			return nil, fmt.Errorf("failed to validate proposals with status = %s: %w", status, err)
 		}
 
 		res = append(res, resp.GetPayload().Proposals...)
--- proposals/repository_test.go
+++ proposals/repository_test.go
@@ -22,6 +22,13 @@ const (
 	fcdTestRejectedProposalID = mocks.TestGetProposalRejectedID
 )
 
+var proposalStatusInputToOutput = map[string]string{
+	"1": "PROPOSAL_STATUS_DEPOSIT_PERIOD",
+	"2": "PROPOSAL_STATUS_VOTING_PERIOD",
+	"3": "PROPOSAL_STATUS_PASSED",
+	"4": "PROPOSAL_STATUS_REJECTED",
+}
+
 func TestGetProposal(t *testing.T) {
 	t.Run("WithMock", func(t *testing.T) {
 		repo := New(&client.TerraRESTApis{Governance: &mocks.TerraGovernanceServiceMock{}})
@@ -59,10 +66,10 @@ func TestGetProposal(t *testing.T) {
 
 func TestFetchVoting(t *testing.T) {
 	t.Run("WithMock", func(t *testing.T) {
-		repo := New(&client.TerraRESTApis{Governance: &mocks.TerraGovernanceServiceMock{}})
-		votings, err := repo.FetchVoting(context.Background())
+		repo := New(&client.TerraRESTApis{Governance: &mocks.TerraGovernanceServiceMock{}, Query: &mocks.TerraQueryServiceMock{}})
+		votes, err := repo.FetchVoting(context.Background())
 		if assert.Nil(t, err) {
-			validateVotingProposals(t, votings)
+			validateVotingProposals(t, votes)
 		}
 	})
 
@@ -78,7 +85,7 @@ func TestFetchVoting(t *testing.T) {
 
 func TestFetchDeposit(t *testing.T) {
 	t.Run("WithMock", func(t *testing.T) {
-		repo := New(&client.TerraRESTApis{Governance: &mocks.TerraGovernanceServiceMock{}})
+		repo := New(&client.TerraRESTApis{Governance: &mocks.TerraGovernanceServiceMock{}, Query: &mocks.TerraQueryServiceMock{}})
 		deposits, err := repo.FetchDeposit(context.Background())
 		if assert.Nil(t, err) {
 			validateDepositProposals(t, deposits)
@@ -142,23 +149,23 @@ func validateGetProposalResult(t *testing.T, proposal *models.GetProposalResult)
 	}
 }
 
-func validateVotingProposals(t *testing.T, proposals []*models.GetProposalListResultProposals) {
+func validateVotingProposals(t *testing.T, proposals []*query.ProposalsOKBodyProposalsItems0) {
 	if assert.NotEmptyf(t, proposals, "voting proposals amount must be more than 0") {
 		t.Logf("validating %d voting proposals", len(proposals))
 		for _, proposal := range proposals {
-			t.Logf("validating voting proposal %s", *proposal.ID)
-			assert.Equalf(t, VotingStatus, *proposal.Status, "FetchVoting list result must be of Voting status")
+			t.Logf("validating voting proposal %s", proposal.ProposalID)
+			assert.Equalf(t, proposalStatusInputToOutput[VotingStatus], proposal.Status, "FetchVoting list result must be of Voting status")
 			validateGetProposalListResult(t, proposal)
 		}
 	}
 }
 
-func validateDepositProposals(t *testing.T, proposals []*models.GetProposalListResultProposals) {
+func validateDepositProposals(t *testing.T, proposals []*query.ProposalsOKBodyProposalsItems0) {
 	if assert.NotEmptyf(t, proposals, "deposit proposals amount must be more than 0") {
 		t.Logf("validating %d deposit proposals", len(proposals))
 		for _, proposal := range proposals {
-			t.Logf("validating deposit proposal %s", *proposal.ID)
-			assert.Equalf(t, DepositStatus, *proposal.Status, "FetchDeposit list result must be of Deposit status")
+			t.Logf("validating deposit proposal %s", proposal.ProposalID)
+			assert.Equalf(t, proposalStatusInputToOutput[DepositStatus], proposal.Status, "FetchDeposit list result must be of Deposit status")
 			validateGetProposalListResult(t, proposal)
 		}
 	}
@@ -167,35 +174,34 @@ func validateDepositProposals(t *testing.T, proposals []*models.GetProposalListR
 // validateGetProposalListResult validates the proposal fields presence in accordance with the
 // specification defined required fields list. Just the same as the validateGetProposalResult, but
 // has a different parameter type.
-func validateGetProposalListResult(t *testing.T, proposal *models.GetProposalListResultProposals) {
-	assert.NotNilf(t, proposal.ID, "proposal ID must not be empty")
-	if assert.NotNilf(t, proposal.Proposer, "proposer must not be empty") {
-		assert.NotNilf(t, proposal.Proposer.AccountAddress, "proposer account address must not be empty")
+func validateGetProposalListResult(t *testing.T, proposal *query.ProposalsOKBodyProposalsItems0) {
+	assert.NotNilf(t, proposal.ProposalID, "proposal proposalID must not be empty")
+
+	if assert.NotNilf(t, proposal.Content, "proposal content must not be empty") {
+		assert.NotNilf(t, proposal.Content.AtType, "proposal atType must not be empty")
+		assert.NotNilf(t, proposal.Content.Title, "proposal title must not be empty")
+		assert.NotNilf(t, proposal.Content.Description, "proposal description must not be empty")
 	}
-	assert.NotNilf(t, proposal.Type, "proposal type must not be empty")
+
 	assert.NotNilf(t, proposal.Status, "proposal status must not be empty")
-	assert.NotNilf(t, proposal.SubmitTime, "proposal submit time must not be empty")
-	assert.NotNilf(t, proposal.Title, "proposal title must not be empty")
-	assert.NotNilf(t, proposal.Description, "proposal description must not be empty")
-	assert.NotNilf(t, proposal.Deposit, "proposal deposit must not be empty")
-	if assert.NotNilf(t, proposal.Vote, "proposal vote must not be empty") {
-		assert.NotNilf(t, proposal.Vote.ID, "vote ID must not be empty")
-		if assert.NotNilf(t, proposal.Vote.Distribution, "proposal votes distribution must not be empty") {
-			assert.NotNilf(t, proposal.Vote.Distribution.Yes, "proposal YES votes distribution must not be empty")
-			assert.NotNilf(t, proposal.Vote.Distribution.No, "proposal NO votes distribution must not be empty")
-			assert.NotNilf(t, proposal.Vote.Distribution.Abstain, "proposal ABSTAIN votes distribution must not be empty")
-			assert.NotNilf(t, proposal.Vote.Distribution.NoWithVeto, "proposal NO WITH VETO votes distribution must not be empty")
-		}
-		if assert.NotNilf(t, proposal.Vote.Count, "proposal votes count must not be empty") {
-			assert.NotNilf(t, proposal.Vote.Count.Yes, "proposal YES votes count must not be empty")
-			assert.NotNilf(t, proposal.Vote.Count.No, "proposal NO votes count must not be empty")
-			assert.NotNilf(t, proposal.Vote.Count.Abstain, "proposal ABSTAIN votes count must not be empty")
-			assert.NotNilf(t, proposal.Vote.Count.NoWithVeto, "proposal NO WITH VETO votes count must not be empty")
-		}
-		assert.NotNilf(t, proposal.Vote.Total, "proposal total votes must not be empty")
-		assert.NotNilf(t, proposal.Vote.VotingEndTime, "proposal voting end time must not be empty")
-		assert.NotNilf(t, proposal.Vote.StakedLuna, "proposal staked luna must not be empty")
+
+	if assert.NotNilf(t, proposal.FinalTallyResult, "proposal final tally result must not be empty") {
+		assert.NotNilf(t, proposal.FinalTallyResult.Yes, "proposal final tally yes result must not be empty")
+		assert.NotNilf(t, proposal.FinalTallyResult.No, "proposal final tally no result must not be empty")
+		assert.NotNilf(t, proposal.FinalTallyResult.Abstain, "proposal final tally abstain result must not be empty")
+		assert.NotNilf(t, proposal.FinalTallyResult.NoWithVeto, "proposal final tally noWithVeto result must not be empty")
 	}
+
+	assert.NotNilf(t, proposal.SubmitTime, "proposal submitTime must not be empty")
+	assert.NotNilf(t, proposal.DepositEndTime, "proposal depositEndTime must not be empty")
+
+	if assert.NotNilf(t, proposal.TotalDeposit, "proposal totalDeposit must not be empty") {
+		assert.NotNilf(t, proposal.TotalDeposit[0].Denom, "proposal totalDeposit must not be empty")
+		assert.NotNilf(t, proposal.TotalDeposit[0].Amount, "proposal totalDeposit amount must not be empty")
+	}
+
+	assert.NotNilf(t, proposal.VotingStartTime, "proposal votingStartTime must not be empty")
+	assert.NotNilf(t, proposal.VotingEndTime, "proposal votingEndTime must not be empty")
 }
 
 // validateGetVotesResult validates each vote fields presence in accordance with the specification
